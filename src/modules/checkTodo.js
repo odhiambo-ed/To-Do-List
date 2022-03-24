530842b0be931d187de210102c280d339c2d20f4
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-24 22:55:50 +0300

    Reset Main

--- src/modules/checkTodo.js
+++
@@ -1,8 +0,0 @@
-const checkTodo = (position, state) => {
-  const getTodos = JSON.parse(localStorage.getItem('todos'));
-  getTodos[position].completed = state;
-  localStorage.setItem('todos', JSON.stringify(getTodos));
-  window.location.reload(true);
-};
-
-export default checkTodo;
