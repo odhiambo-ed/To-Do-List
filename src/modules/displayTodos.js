734b2bad6f00a2e93b30d4a0d6efed086853f867
Edward Odhiambo <odhiambo.edward111@gmail.com>
2022-03-24 15:21:26 +0300

    Add Interactive Function

--- src/modules/displayTodos.js
+++ src/modules/displayTodos.js
@@ -1,10 +1,22 @@
+import Sortable from 'sortablejs';
+
 import checkTodo from './checkTodo.js';
 import deleteTodo from './deleteTodo.js';
 
 const displayItems = (position, item) => {
   const tableBody = document.getElementById('tbody');
+  Sortable.create(tableBody, {
+    onEnd: (event) => {
+      const existentTodos = JSON.parse(localStorage.getItem('todos'));
+      const tmp = existentTodos[event.newIndex];
+      existentTodos[event.newIndex] = item;
+      existentTodos[event.oldIndex] = tmp;
+      localStorage.setItem('todos', JSON.stringify(existentTodos));
+      window.location.reload(true);
+    },
+  });
   const tableRow = document.createElement('tr');
-  tableRow.style.cursor = 'pointer';
+  tableRow.style.cursor = 'move';
   const deleteButton = document.createElement('i');
   deleteButton.setAttribute('class', 'bi bi-trash3-fill');
   deleteButton.setAttribute('id', 'end-delete');
@@ -12,7 +24,8 @@ const displayItems = (position, item) => {
   todoInput.setAttribute('class', item.completed && 'completed');
   todoInput.setAttribute('type', 'text');
   todoInput.setAttribute('value', item.todo);
-  todoInput.setAttribute('disabled', 'true');
+  todoInput.disabled = true;
+  todoInput.setAttribute('id', 'todo-edit');
   todoInput.style.border = 'none';
   todoInput.style.outline = 'none';
   todoInput.style.paddingLeft = 200;
@@ -33,6 +46,7 @@ const displayItems = (position, item) => {
     todoInput.style.backgroundColor = '';
   });
   const th1 = document.createElement('td');
+  th1.draggable = true;
   th1.style.flex = 1;
   th1.style.width = '100%';
   const checkInput = document.createElement('input');
@@ -48,15 +62,30 @@ const displayItems = (position, item) => {
   });
   th1.appendChild(checkInput);
   const th2 = document.createElement('td');
-  th2.appendChild(todoInput);
+  th2.draggable = true;
+  const actionForm = document.createElement('form');
+  actionForm.appendChild(todoInput);
+  actionForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const listedTodos = JSON.parse(localStorage.getItem('todos'));
+    listedTodos[position].todo = todoInput.value;
+    localStorage.setItem('todos', JSON.stringify(listedTodos));
+    window.location.reload(true);
+  });
+  th2.appendChild(actionForm);
   const th3 = document.createElement('td');
   th3.appendChild(deleteButton);
   const th4 = document.createElement('td');
-  th4.innerHTML = `
-    <div class="end-dots">
-      <i class="bi bi-three-dots-vertical"></i>
-    </div>
-  `;
+  const dotDiv = document.createElement('div');
+  dotDiv.setAttribute('class', 'end-dots');
+  const dotIcon = document.createElement('i');
+  dotIcon.setAttribute('class', 'bi bi-three-dots-vertical');
+  dotIcon.setAttribute('id', 'dot-icon');
+  dotIcon.addEventListener('click', () => {
+    todoInput.disabled = false;
+  });
+  dotDiv.appendChild(dotIcon);
+  th4.appendChild(dotDiv);
   tableRow.appendChild(th1);
   tableRow.appendChild(th2);
   tableRow.appendChild(th3);
